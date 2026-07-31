43d701ca464db77b3f238ad1ac859bf2bd0fa333
itznikhilyadav <itsnikhil.tech9@gmail.com>
2026-07-31 11:00:24 +0530

    Configure PostgreSQL database

--- config/settings.py
+++ config/settings.py
@@ -1,4 +1,5 @@
 import os
+import dj_database_url
 from pathlib import Path
 from dotenv import load_dotenv
 
@@ -111,10 +112,11 @@ WSGI_APPLICATION = "config.wsgi.application"
 # Database
 # --------------------------------------------------
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    "default": dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+        conn_max_age=600,
+        conn_health_checks=True,
+    )
 }
 
 # --------------------------------------------------
